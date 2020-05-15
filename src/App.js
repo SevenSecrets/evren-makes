46857deafdf5f9890826a3511e1b88a5160b0609
SevenSecrets <freddieshughes@googlemail.com>
2020-05-15 15:55:37 +0100

    Content - adding links to socials, removing unused react logo pngs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
         <h1>
           Evren Makes
         </h1>
+        <div className="Socials">
+          <a href="https://instagram.com/evrenmakes" target="_blank" rel="noopener noreferrer">
+            Instagram
+          </a>
+          <a href="https://twitter.com/BhEvren" target="_blank" rel="noopener noreferrer">
+            Twitter
+          </a>
+        </div>
       </header>
       <body className="App-body">
         <p>
